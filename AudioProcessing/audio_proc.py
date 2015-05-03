9f878a0fd270ff2878abb000e015801fdf59a8ed
William Fong <trigition@gmail.com>
2015-05-02 18:12:41 -0600

    Implemented more audio processing, adding more control now

--- AudioProcessing/audio_proc.py
+++ AudioProcessing/audio_proc.py
@@ -2,72 +2,107 @@
 __author__ = 'William'
 
 from scipy.io import wavfile as wav
+import wave
+import struct
 import numpy as np
-from math import ceil, log10
-from pylab import *
+import matplotlib.pylab as plt
+
 
 class Audio_Factory():
-    def __init__(self, file_path, binary_size):
+    def __init__(self, file_path, range_columns):
         """Constructor for an Audio Factory"""
+        # Load file statistics
         self.file_path = file_path
-        self.sample_rate, self.samples = wav.read(file_path)
-        self.bin_size = binary_size
-        self.transform = None
-        self.right_transform = None
-        self.left_transform = None
-        self.num_unique_points = None
-        # print self.samples
+        self.sound_file = wave.open(file_path, 'r')
+        self.file_size = self.sound_file.getnframes()
+        self.sample_rate = self.sound_file.getframerate()
+        self.sample_width = self.sound_file.getsampwidth()
+        self.sample_duration = self.file_size / float(self.sample_rate)
+        # Open sound file using file attribtues
+        print "Opening:", self.file_path, " with: "
+        print "Size:\t", self.file_size
+        print "Sample Rate:\t", self.sample_rate
+        print "Sample Width\t", self.sample_width
+        print "Sample Duration\t", self.sample_duration
+        self.sound_data = self.sound_file.readframes(self.file_size)
+        self.sound_file.close()
+        # Interpret file data into useful information
+        unpack_fmt = '%dh' % self.file_size * 2
+        print unpack_fmt
+        self.sound_data = struct.unpack(unpack_fmt, self.sound_data)
+        # Perform Fourier Analysis
+        self.fouriers_per_second = 24 # Frames per second
+        self.fourier_spread = 1.0 / self.fouriers_per_second
+        self.fourier_width = self.fourier_spread
+        self.fourier_width_index = self.fourier_width * float(self.sample_rate)
+
+        process_length = self.sample_duration - 1
+        self.total_transforms = int(round(process_length * self.fouriers_per_second))
+        self.fourier_spacing = self.fourier_width * float(self.sample_rate)
+
+        self.last_point = int(round(process_length * float(self.sample_rate) + self.fourier_width_index)) - 1
+        self.frequency = self.sample_rate / self.fourier_width_index * np.arange(self.fourier_width_index)
+
+        self.fft_bands = range_columns
+        self.x_axis = range(0, range_columns)
+        self.fft_averages = []
+
+        # Print updates
+        print "Duration:", self.sample_duration
+        print "For fourier width of:", self.fourier_width, ", need:", self.fourier_width_index, "samples each FFT"
+        print "Transformations per second:", self.fouriers_per_second
+        print "Total fouriers:", self.total_transforms * self.fourier_spread
+        print "Spacing:", self.fourier_spacing
+        print "Total transformations:", self.total_transforms
+
+    def get_bandwidth(self):
+        return (2.0 / self.sample_rate) * (self.fourier_width_index / 2.0)
+
+    def frequency_to_index(self, frequency):
+        if frequency < self.get_bandwidth():
+            return 0 #Frequency is outside of bounds
+        if frequency > (self.sample_rate / 2 - self.get_bandwidth() / 2):
+            return self.fourier_width_index - 1
+        
+        fraction = float(frequency) / float(self.sample_rate)
+        index = round(self.fourier_width_index * fraction)
+        return index
+
+    def average_fft_bands(self, fft_array):
+        del self.fft_averages[:]
+        for band in self.x_axis:
+            average = 0.0
+            if band == 0:
+                low_freq = 0
+            else:
+                low_freq = int(int(self.sample_rate / 2) / float(2 ** (self.fft_bands - band)))
+            high_freq = int((self.sample_rate / 2) / float(2 ** ((self.fft_bands - 1) - band)))
+            low_bound = int(self.frequency_to_index(low_freq))
+            high_bound = int(self.frequency_to_index(high_freq))
+            print "Low:", low_bound, ", High:", high_bound
+            for j in range(low_bound, high_bound):
+                average += fft_array[j]
+            average /= (high_bound - low_bound + 1)
+            self.fft_averages.append(average)
 
     def process_audio(self):
-        # Pad Audio Signal with zeros
-        samples = np.append(np.zeros(np.floor(self.bin_size/2.0)), self.samples)
-        # Obtain Frames for each time sample
-        frames = lib.stride_tricks.as_strided()
-        print samples
+        for offset in range(0, self.total_transforms):
+            start = int(offset * self.fourier_width_index)
+            end = int((offset * self.fourier_width_index) + self.fourier_width_index - 1)
 
-    def take_real_fourier(self):
-        """Create fourier transform arrays of each channel"""
-        print "Taking Real Fourier Time Transform of:", self.file_path
-        left_transform = np.fft.rfft(self.get_left_channel())
-        right_transform = np.fft.rfft(self.get_right_channel())
-        print "Took transforms of left and right channels"
+            process_range = self.sound_data[start:end]
+            # Actually perform Fourier Time Transform
+            fft_data = abs(np.fft.fft(process_range))
+            fft_data *= ((2 ** 0.5) / self.fourier_width_index)
 
-        # Normalize and standardize waveform
-        print "Normalizing channels"
-        self.num_unique_points = ceil((len(self.samples) + 1) / 2.0)
-        # Ensure that transform is same duration of audio file
-        left_transform = left_transform[0:self.num_unique_points]
-        right_transform = right_transform[0:self.num_unique_points]
-        # Find the power(dB) of each transform
-        # Do it for Left Channel
-        print "Finding power (dB) values of left and right channels"
-        left_transform = abs(left_transform)
-        left_transform /= float(self.num_unique_points)
-        left_transform = left_transform ** 2 # Transform units are now in dB (Power)
-        # Do it for Right Channel
-        right_transform = abs(right_transform)
-        right_transform /= float(self.num_unique_points)
-        right_transform = right_transform ** 2 # Transform units are now in dB (Power)
-
-        # Multiply by 2
-        odd = self.num_unique_points % 2
-        left_transform[1:len(self.num_unique_points) - odd] = left_transform[1:len(self.num_unique_points) - odd] * 2
-        right_transform[1:len(self.num_unique_points) - odd] = right_transform[1:len(self.num_unique_points) - odd] * 2
-
-        # Set values to object instance
-        self.right_transform = right_transform
-        self.left_transform = left_transform
-        print "Successfully performed transform on:", self.file_path
-
-    def plot_transforms(self):
-        """Plots the Fourier Transform to a plot (Debugging)"""
-        print "Creating plot"
-
-
-    def get_right_channel(self):
-        """Returns the right channel samples"""
-        return self.samples[:, 0]
-
-    def get_left_channel(self):
-        """Returns the left channel samples"""
-        return self.samples[0,:]
+            # IMPLEMENT NEURAL MATRIX OUTPUT
+            # Plot
+            plt.ylim(0, 1000)
+            self.average_fft_bands(fft_data)
+            y_axis = self.fft_averages
+            width = 0.35
+            p1 = plt.bar(self.x_axis, y_axis, width, color='r')
+            # Print plot to file
+            output = str('frame_%05d' % offset) + '.png'
+            plt.savefig(output, dpi=300)
+            plt.close
--- AudioProcessing/main_processor.py
+++ AudioProcessing/main_processor.py
@@ -3,5 +3,5 @@ __author__ = 'William'
 
 from audio_proc import Audio_Factory
 
-test = Audio_Factory('test.wav', 1024)
+test = Audio_Factory('test.wav', 24)
 test.process_audio()

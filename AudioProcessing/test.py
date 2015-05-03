86bf21c2ec33108f9771eec8a2241d2ec1459204
William Fong <trigition@gmail.com>
2015-05-03 13:21:36 -0600

    RFFT now printed in Numpy Memory Dump

--- AudioProcessing/test.py
+++ AudioProcessing/test.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python
 
-import pylab
-from numpy import arange
-from scipy.io import wavfile
+import wave
+import struct
+import matplotlib.pyplot as plt
+import numpy as np
 
-files = 'AudioFiles/New Avengers.wav'
-sample_frequency, snd = wavfile.read('test.wav')
-print "File Data Type:", snd.dtype
-# Convert Signal to floating point within range of snd.dtype
-snd /= (2.**15)
-# print snd.shape[0]
-# print snd.shape[1]
-duration = snd.shape[0] / sample_frequency
-s1 = snd[:, 0]
-time_array = arange(0, snd.shape[0], 1)
-time_array /= sample_frequency
-time_array *= 1000
+filename = 'test.wav'
 
+sound_file = wave.open(filename)
+channels = sound_file.getnchannels()
+audio_width = sound_file.getsampwidth()
+n_frames = sound_file.getnframes()
+frame_rate = sound_file.getframerate()
+
+# Read in Audio Signal and close file
+
+duration = n_frames / frame_rate
+audio_signal = sound_file.readframes(n_frames)
+sound_file.close()
+
+# Convert Audio Signal to usable Data
+
+print "Unpacking Data"
+unpack_format = '%dh' % (n_frames * 2)
+audio_data = struct.unpack(unpack_format, audio_signal)
+print "Data Unpacked"
+print "Transforming Audio Data to NP Array"
+audio_data = np.array(audio_data, dtype='h')
+print "Performing RFFT Transform"
+fourier_trans = np.fft.rfft(audio_data)
+print "Performed RFFT Transform"
+fourier_trans.dump('RTTF_' + filename)
+# Plot Audio Data
+"""
+plt.figure(1)
+plt.title('Yolo')
+plt.plot(raw_data)
+plt.show()"""
+# Plot FFT Data
+"""
+plt.figure(1)
+plt.title('FFT')
+plt.plot(fourier_trans)
+plt.show()"""

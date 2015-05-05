19a973e9bd5fd0071bce3a802fb1cf6a006b45ce
Skyler <skymanzanares@hotmail.com>
2015-05-04 21:22:36 -0600

    Added optional second parameter to script to make it a little easier to use

--- BackPropogation/format.sh
+++ BackPropogation/format.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo usage: $0 directory
+	echo usage: $0 directory "[binDir]"
 	exit
 fi
 
 wd=`pwd`
 exten=".arff"
+output=$wd\formattedJerks
+
+if [ ! -z "$2" ]; then
+	#this means we have a second parameter
+	#which means we have a different bin than normal
+	echo Changing output directory to $2
+	output=$2
+fi
 
 cd $1
 
